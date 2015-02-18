b899c2aaffa163c610702b0ef90c187ec371c42f
Marc Charbonneau <marc@mbcharbonneau.com>
2015-02-18 10:09:24 -0800

    New project.

---
+++ BeaconSim/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeaconSim
+//
+//  Created by mbcharbonneau on 2/18/15.
+//  Copyright (c) 2015 Geoloqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BeaconSim/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BeaconSim
+//
+//  Created by mbcharbonneau on 2/18/15.
+//  Copyright (c) 2015 Geoloqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
